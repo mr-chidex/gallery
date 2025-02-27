c31d698e4cf918c99155831d4e49d7ecca65f485
mr-chidex <mbahchiboy2@gmail.com>
2025-02-27 18:12:13 +0100

    init app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <h1>Header Text</h1>
+  </header>
+  <main>
+    <h1>Home</h1>
+  </main>
+</template>
+
+<style>
+.header {
+  background: #eee;
+}
+</style>
