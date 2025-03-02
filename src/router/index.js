eeb6046f38dd7f651f3f08bc974464514b3b139a
mr-chidex <mbahchiboy2@gmail.com>
2025-03-02 21:51:52 +0100

    not found page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: "/search",
       name: "search",
       component: () => import("../views/SearchView.vue"),
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "notFound",
+      component: NotFound,
     },
   ],
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Wrapper from "@/components/Wrapper.vue";
+</script>
+
+<template>
+  <Wrapper>
+    <template #header>
+      <h1>Page Not Found</h1>
+    </template>
+
+    <template #main>
+      <div class="not-found">
+        <p>The page you're looking for doesn't exist.</p>
+        <router-link to="/">Back to Home</router-link>
+      </div>
+    </template>
+  </Wrapper>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: #15054f;
+  text-align: left;
+}
+.not-found {
+  margin-top: 6rem;
+  display: grid;
+  gap: 1rem;
+}
+</style>
