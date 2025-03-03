c7f4eb45013a07ce10d976135c7c8d225ee2a419
mr-chidex <mbahchiboy2@gmail.com>
2025-03-03 11:06:28 +0100

    loader added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
+  <Loader v-if="isLoading" />
   <RouterView />
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref, onMounted } from "vue";
+import Loader from "@/components/Loader.vue";
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 1000);
+});
+</script>
---
+++ src/components/Loader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-if="isLoading" class="preloader">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 2000);
+});
+</script>
+
+<style scoped>
+.preloader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+  transition: opacity 0.3s ease-out;
+}
+
+.spinner {
+  width: 3.125rem;
+  height: 3.125rem;
+  border: 0.3125rem solid rgba(0, 0, 0, 0.1);
+  border-top: 0.3125rem solid white;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
